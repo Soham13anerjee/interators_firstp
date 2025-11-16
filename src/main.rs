0a17e768ec6043e794b6f1b7f76c622a12eaeddd
Soham13anerjee <sohambanerg@gmail.com>
2025-11-16 11:25:28 +0530

    Pushing code version 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+struct Counter{
+    count:u32,
+}
+
+trait Iterator {
+    fn next(&mut self) -> Option<u32>;
+}
+
+impl Counter {
+    fn new() -> Self {
+        Self { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    fn next(&mut self) -> Option<u32> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter = Counter::new();
+
+    // while let Some(number) = counter.next() {
+    //     println!("{}", number);
+    // } 
+    loop{
+        match counter.next(){
+            Some(number) => println!("{}",number),
+            None => break,
+        }
+    }
 }
